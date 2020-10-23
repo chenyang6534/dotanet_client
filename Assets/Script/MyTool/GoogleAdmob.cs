daf720e063505861ed6f4ef6abb0a1abdca25c8a
chenyang <357328436@qq.com>
2020-10-23 11:01:48 +0800

    加入googlead

--- Assets/Script/MyTool/GoogleAdmob.cs
+++ Assets/Script/MyTool/GoogleAdmob.cs
@@ -1,14 +1,14 @@
 ﻿using cocosocket4unity;
-//using GoogleMobileAds.Api;
+using GoogleMobileAds.Api;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Advertisements;
 
-public class GoogleAdmob : MonoBehaviour,IUnityAdsListener
+public class GoogleAdmob : MonoBehaviour, IUnityAdsListener
 {
-    //private RewardedAd rewardedAd;
+    private RewardedAd rewardedAd;
     // Use this for initialization
     public static GoogleAdmob Instanse = null;
 
@@ -24,9 +24,10 @@ public class GoogleAdmob : MonoBehaviour,IUnityAdsListener
     public string loaderrormsg;
     void Start()
     {
-        
-        if (GoogleAdmob.Instanse == null){
-            //MobileAds.Initialize(initStatus => { });
+
+        if (GoogleAdmob.Instanse == null)
+        {
+            MobileAds.Initialize(initStatus => { });
         }
 
         MsgManager.Instance.AddListener("SC_UseWatchVedioItem", new HandleMsg(this.SC_UseWatchVedioItem));
@@ -49,7 +50,7 @@ public class GoogleAdmob : MonoBehaviour,IUnityAdsListener
 
     public void OnUnityAdsDidError(string message)
     {
-        Debug.Log("OnUnityAdsDidError:"+message);
+        Debug.Log("OnUnityAdsDidError:" + message);
     }
 
     public void OnUnityAdsDidStart(string placementId)
@@ -59,8 +60,8 @@ public class GoogleAdmob : MonoBehaviour,IUnityAdsListener
 
     public void OnUnityAdsDidFinish(string placementId, ShowResult showResult)
     {
-        Debug.Log("OnUnityAdsDidFinish:"+ showResult);
-        if(showResult == ShowResult.Finished)
+        Debug.Log("OnUnityAdsDidFinish:" + showResult);
+        if (showResult == ShowResult.Finished)
         {
             if (GoogleAdmob.CurRewardVideoPlay != null)
             {
@@ -71,96 +72,96 @@ public class GoogleAdmob : MonoBehaviour,IUnityAdsListener
         {
 
         }
-        
+
     }
 
     public void CreateAndLoadRewardedAd()
     {
-//#if UNITY_ANDROID
-//        string adUnitId = "ca-app-pub-3940256099942544/5224354917";
-//#elif UNITY_IPHONE
-//        //string adUnitId = "ca-app-pub-3940256099942544/1712485313";//测试
-//        //string adUnitId = "ca-app-pub-3664269345038379/6857936057";//正式ca-app-pub-3664269345038379/6857936057
-//        string adUnitId = "ca-app-pub-3664269345038379/8895145951";
-//        Debug.Log("    正式ca-app-pub-3664269345038379/8895145951");
-//#else
-//        string adUnitId = "unexpected_platform";
-//#endif
+#if UNITY_ANDROID
+        string adUnitId = "ca-app-pub-3940256099942544/5224354917";
+#elif UNITY_IPHONE
+        //string adUnitId = "ca-app-pub-3940256099942544/1712485313";//测试
+        //string adUnitId = "ca-app-pub-3664269345038379/6857936057";//正式ca-app-pub-3664269345038379/6857936057
+        string adUnitId = "ca-app-pub-3664269345038379/8895145951";
+        Debug.Log("    正式ca-app-pub-3664269345038379/8895145951");
+#else
+        string adUnitId = "unexpected_platform";
+#endif
 
-//        this.rewardedAd = new RewardedAd(adUnitId);
+        this.rewardedAd = new RewardedAd(adUnitId);
 
-//        // Called when an ad request has successfully loaded.
-//        this.rewardedAd.OnAdLoaded += HandleRewardedAdLoaded;
-//        // Called when an ad request failed to load.
-//        this.rewardedAd.OnAdFailedToLoad += HandleRewardedAdFailedToLoad;
-//        // Called when an ad is shown.
-//        this.rewardedAd.OnAdOpening += HandleRewardedAdOpening;
-//        // Called when an ad request failed to show.
-//        this.rewardedAd.OnAdFailedToShow += HandleRewardedAdFailedToShow;
-//        // Called when the user should be rewarded for interacting with the ad.
-//        this.rewardedAd.OnUserEarnedReward += HandleUserEarnedReward;
-//        // Called when the ad is closed.
-//        this.rewardedAd.OnAdClosed += HandleRewardedAdClosed;
+        // Called when an ad request has successfully loaded.
+        this.rewardedAd.OnAdLoaded += HandleRewardedAdLoaded;
+        // Called when an ad request failed to load.
+        this.rewardedAd.OnAdFailedToLoad += HandleRewardedAdFailedToLoad;
+        // Called when an ad is shown.
+        this.rewardedAd.OnAdOpening += HandleRewardedAdOpening;
+        // Called when an ad request failed to show.
+        this.rewardedAd.OnAdFailedToShow += HandleRewardedAdFailedToShow;
+        // Called when the user should be rewarded for interacting with the ad.
+        this.rewardedAd.OnUserEarnedReward += HandleUserEarnedReward;
+        // Called when the ad is closed.
+        this.rewardedAd.OnAdClosed += HandleRewardedAdClosed;
 
-//        // Create an empty ad request.
-//        AdRequest request = new AdRequest.Builder().Build();
-//        // Load the rewarded ad with the request.
-//        this.rewardedAd.LoadAd(request);
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+        // Load the rewarded ad with the request.
+        this.rewardedAd.LoadAd(request);
     }
-    //public void HandleRewardedAdLoaded(object sender, EventArgs args)
-    //{
-    //    Debug.Log("HandleRewardedAdLoaded event received");
-    //}
+    public void HandleRewardedAdLoaded(object sender, EventArgs args)
+    {
+        Debug.Log("HandleRewardedAdLoaded event received");
+    }
 
-    //public void HandleRewardedAdFailedToLoad(object sender, AdErrorEventArgs args)
-    //{
-    //    Debug.Log(
-    //        "HandleRewardedAdFailedToLoad event received with message: "
-    //                         + args.Message);
-    //    loaderrormsg = args.Message;
-    //}
+    public void HandleRewardedAdFailedToLoad(object sender, AdErrorEventArgs args)
+    {
+        Debug.Log(
+            "HandleRewardedAdFailedToLoad event received with message: "
+                             + args.Message);
+        loaderrormsg = args.Message;
+    }
 
-    //public void HandleRewardedAdOpening(object sender, EventArgs args)
-    //{
-    //    Debug.Log("HandleRewardedAdOpening event received");
-    //}
+    public void HandleRewardedAdOpening(object sender, EventArgs args)
+    {
+        Debug.Log("HandleRewardedAdOpening event received");
+    }
 
-    //public void HandleRewardedAdFailedToShow(object sender, AdErrorEventArgs args)
-    //{
-    //    Debug.Log(
-    //        "HandleRewardedAdFailedToShow event received with message: "
-    //                         + args.Message);
-    //}
-    //public void HandleUserEarnedReward(object sender, Reward args)
-    //{
-    //    string type = args.Type;
-    //    double amount = args.Amount;
-    //    Debug.Log(
-    //        "HandleRewardedAdRewarded event received for "
-    //                    + amount.ToString() + " " + type);
-    //    //Protomsg.CS_LookVedioSucc msg1 = new Protomsg.CS_LookVedioSucc();
-    //    //msg1.ID = 1;
-    //    //MyKcp.Instance.SendMsg(GameScene.Singleton.m_ServerName, "CS_LookVedioSucc", msg1);
-    //    if (GoogleAdmob.CurRewardVideoPlay != null)
-    //    {
-    //        GoogleAdmob.CurRewardVideoPlay(true, false, false);
-    //    }
+    public void HandleRewardedAdFailedToShow(object sender, AdErrorEventArgs args)
+    {
+        Debug.Log(
+            "HandleRewardedAdFailedToShow event received with message: "
+                             + args.Message);
+    }
+    public void HandleUserEarnedReward(object sender, Reward args)
+    {
+        string type = args.Type;
+        double amount = args.Amount;
+        Debug.Log(
+            "HandleRewardedAdRewarded event received for "
+                        + amount.ToString() + " " + type);
+        //Protomsg.CS_LookVedioSucc msg1 = new Protomsg.CS_LookVedioSucc();
+        //msg1.ID = 1;
+        //MyKcp.Instance.SendMsg(GameScene.Singleton.m_ServerName, "CS_LookVedioSucc", msg1);
+        if (GoogleAdmob.CurRewardVideoPlay != null)
+        {
+            GoogleAdmob.CurRewardVideoPlay(true, false, false);
+        }
 
-    //}
+    }
     public void HandleRewardedAdClosed(object sender, EventArgs args)
     {
         this.CreateAndLoadRewardedAd();
     }
     public void UserChoseToWatchAd()
     {
-        //if (this.rewardedAd.IsLoaded())
-        //{
-        //    this.rewardedAd.Show();
-        //}
-        //else
-        //{
-        //    this.CreateAndLoadRewardedAd();
-        //}
+        if (this.rewardedAd.IsLoaded())
+        {
+            this.rewardedAd.Show();
+        }
+        else
+        {
+            this.CreateAndLoadRewardedAd();
+        }
     }
 
     public int WatchVedioState = 0;//0无 1请求服务器中
@@ -188,23 +189,21 @@ public class GoogleAdmob : MonoBehaviour,IUnityAdsListener
     }
     public void ShowVedio2(EndPlayRewardVideoResult ep)
     {
-        //if (this.rewardedAd.IsLoaded() == false)
-        if(true)
+        if (this.rewardedAd.IsLoaded() == false)
         {
             Debug.Log("请先加载广告");
-            //this.CreateAndLoadRewardedAd();
+            this.CreateAndLoadRewardedAd();
 
 
 
             //显示unity广告
-            if(Advertisement.isInitialized && Advertisement.IsReady(UnityADPlacementId))
+            if (Advertisement.isInitialized && Advertisement.IsReady(UnityADPlacementId))
             {
                 Advertisement.Show(UnityADPlacementId);
                 GoogleAdmob.CurRewardVideoPlay = ep;
             }
             else
             {
-                Debug.Log("请先加载广告");
                 UMengManager.Instanse.Event_watch_vedio(this.loaderrormsg);
                 new NoVedioNotice(this.loaderrormsg, (code) =>
                 {
@@ -221,14 +220,15 @@ public class GoogleAdmob : MonoBehaviour,IUnityAdsListener
                     }
                 });
             }
-            
+
 
             //TTadMgr.Instanse.LoadRewardAd();
             return;
         }
         else
         {
-            //this.rewardedAd.Show();
+            Debug.Log("google ad show succ");
+            this.rewardedAd.Show();
             GoogleAdmob.CurRewardVideoPlay = ep;
         }
 
@@ -278,5 +278,5 @@ public class GoogleAdmob : MonoBehaviour,IUnityAdsListener
 
     }
 
-    
+
 }
